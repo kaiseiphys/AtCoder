272d8668aca586414ada3f53ebfefbd7080827e3
Kaisei <kaisei.2482480501@icloud.com>
2022-10-19 00:41:45 +0900

    add ABC251 all

---
+++ ABC251/ABC251B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int counter = 0;
+    int n, w;
+    cin >> n >> w;
+
+    vector <int> A(n,0);
+    for (int i=0; i<n; i++) {
+        cin >> A.at(i);
+    }
+
+    // for (int i=0; i<n; i++) {
+    //     cout << A.at(i) << endl;
+    // }
+
+    for (int i=1; i<n; i++) {
+
+    } 
+
+}
